d7dfe2359bfba73f95f03a740a3aadece73a90f3
Stanley <scohe001@ucr.edu>
2017-05-01 19:39:59 -0700

    Got the rpi and the arduino talking

--- arduino/serial_test/serial_test.ino
+++ arduino/serial_test/serial_test.ino
@@ -8,7 +8,7 @@ void setup() {
 void loop() {
   a++;                          // a value increase every loop
   sprintf(dataString,"%02X",a); // convert a value to hexa 
-  Serial.println(dataString);   // send the data
-  delay(1000);                  // give the loop some break
+  Serial.println("Hey there!");   // send the data
+  delay(5000);                  // give the loop some break
 
 }
