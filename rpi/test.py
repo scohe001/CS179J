1d7362976cc47044a4f639798618ae28d8ed0615
Stanley <scohe001@ucr.edu>
2017-05-06 17:44:33 -0700

    Made some minor changes. Moving to c9 to make things easier

--- rpi/test.py
+++ rpi/test.py
@@ -1,10 +1,11 @@
 import threading
 import time
 import getpass
+import pyttsx
 from firebase import Firebase
 #https://pypi.python.org/pypi/python-firebase/1.2
 
-
+engine = pyttsx.init()
 fire_url = 'https://smart-fridge-76b1c.firebaseio.com'
 
 fireserial = Firebase(fire_url+'/Serials')
@@ -28,24 +29,33 @@ def update_db():
         infridge = firein.get()
         outfridge = fireout.get()
         
-        time.sleep(.5)
+        time.sleep(15)
+
 
 def get_serial():
+    global serials, infridge, outfridge
+    global fireserial, firein, fireout
     global running
     
     while running:
         scanned = getpass.getpass('').decode('utf-8')
         if scanned == 'stop'.decode('utf-8'): running = False
         elif scanned in serials:
-            print "You scanned " + serials[scanned]
+            print "Scanned " + serials[scanned]
+            #engine.say(str("Test test test"))
+            #engine.runAndWait()
             
             if scanned in outfridge:
                 Firebase(fire_url+'/Taken Out/'+scanned).delete()
                 Firebase(fire_url+'/In Fridge/').patch({scanned:True})
+                infridge[scanned] = outfridge[scanned]
+                outfridge.pop(scanned)
                 print "Putting it back in..."
             elif scanned in infridge: 
                 Firebase(fire_url+'/In Fridge/'+scanned).delete()
                 Firebase(fire_url+'/Taken Out/').patch({scanned:True})
+                outfridge[scanned] = infridge[scanned]
+                infridge.pop(scanned)
                 print "Taking it out..."
             else: 
                 Firebase(fire_url+'/In Fridge/').patch({scanned:True})
