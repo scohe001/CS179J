11c01e17ce62fcdeb757bf013346e21612221fe9
Stanley <scohe001@ucr.edu>
2017-05-06 17:22:56 -0700

    Added some code to test IR

---
+++ arduino/IR_sensor/IR_sensor.ino
@@ -0,0 +1,24 @@
+char dataString[50] = {0};
+int a = 0, BUTTON = 8; 
+
+void setup() {
+  pinMode(BUTTON, INPUT);
+  digitalWrite(BUTTON, HIGH);
+  Serial.begin(9600);              //Starting serial communication
+}
+
+bool pressed = false;
+
+void loop() {
+  
+  if(digitalRead(BUTTON) == LOW) { //Low is pressed for buttons
+    if(!pressed) {
+      pressed = true;
+      Serial.println(1);
+    }
+  } else {
+    pressed = false;
+  }
+  
+  delay(20);
+}
