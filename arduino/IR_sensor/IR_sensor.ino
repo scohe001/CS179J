a0814bedd4ed3f1005402550b788090e1c5d9d13
Ace Rhino <a12n@mac.com>
2017-06-12 20:33:41 -0700

    Finished report and added gitignore

--- arduino/IR_sensor/IR_sensor.ino
+++ arduino/IR_sensor/IR_sensor.ino
@@ -3,14 +3,17 @@ int a = 0, BUTTON = 8;
 
 void setup() {
   pinMode(BUTTON, INPUT);
+  pinMode(7, INPUT);
+  digitalWrite(7, HIGH);
   digitalWrite(BUTTON, HIGH);
   Serial.begin(9600);              //Starting serial communication
 }
 
 bool pressed = false;
+bool pressed2 = false;
 
 void loop() {
-  Serial.println(digitalRead(BUTTON));
+  //Serial.println(digitalRead(BUTTON));
   if(digitalRead(BUTTON) == LOW) { //Low is pressed for buttons
     if(!pressed) {
       pressed = true;
@@ -20,5 +23,23 @@ void loop() {
     pressed = false;
   }
   
+  if(digitalRead(7) == LOW) {
+    if(!pressed2) {
+      pressed2 = true;
+      Serial.println(2);
+    }
+  } else {
+    pressed2 = false;
+  }
+  
+  if (Serial.available() > 0) {
+    String incoming = Serial.readString();
+    
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(incoming);
+  }
+  
   delay(20);
 }
+
